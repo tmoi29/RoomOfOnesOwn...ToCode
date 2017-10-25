bf3acf40432a14aa8b919cb0cbf3ede64d77b062
Irene Lam <ilam1@stuy.edu>
2017-10-25 00:20:57 -0400

    Added templates, welcome page, navigation bar, stylistic features

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def hello_world():
     Otherwise, the login page is displayed
     '''
     if "username" in session.keys():
-       return render_template("loggedin.html", name = session["username"])
+       return render_template("welcome.html", name = session["username"])
     return render_template("login.html", message = "")
 
-@app.route("/loggedin")
+@app.route("/welcome")
 def logged_in():
    input_name = request.args["username"]
    input_pass = request.args["password"]
@@ -26,11 +26,21 @@ def logged_in():
    if input_name == the_username:
       if input_pass == the_password:
          session["username"] = input_name #Creates a new session
-         return render_template("loggedin.html", name = input_name)
+         return render_template("welcome.html", name = input_name)
       else:
          return render_template("login.html", message = "Error: Wrong password")
    else:
       return render_template("login.html", message =  "Error: Wrong username")
+
+@app.route("/profile")
+def profile():
+    #To be updated
+    return render_template("profile.html")
+
+@app.route("/about")
+def about():
+    #To be updated
+    return render_template("about.html")
 
 @app.route("/logout")
 def logged_out():
