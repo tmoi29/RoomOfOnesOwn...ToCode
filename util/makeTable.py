a5efb48c16967974153d5d4e85aae683384e8060
tmoi29 <tmoi@stuy.edu>
2017-10-26 14:41:48 -0400

    issues with time

--- util/makeTable.py
+++ util/makeTable.py
@@ -13,8 +13,10 @@ def db_setup():
 	c = sqlite3.connect("info.db").cursor()
 #	c.execute("SET sql_notes = 0;")
 	c.execute("CREATE TABLE IF NOT EXISTS accounts(username TEXT PRIMARY KEY, pass TEXT);")
-	c.execute("CREATE TABLE IF NOT EXISTS blogs(id INTEGER PRIMARY KEY, username TEXT, title TEXT, body TEXT, timestamp TEXT);")
+	c.execute("CREATE TABLE IF NOT EXISTS blogs(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, title TEXT, body TEXT, timestamp TEXT);")
 #	c.execute("SET sql_notes = 1;")
+
+#db_setup()
 
 #Accounts
 #==========================================================
@@ -24,9 +26,12 @@ c.execute(command)    #run SQL statement
 """
 #Create an account
 def createAcc(user, passw):
-	c_dup = db.cursor()
-	command = "INSERT INTO accounts VALUES(%s, %s)" %(user,passw)
-	c_dup.execute(command)
+        try:
+	        c_dup = db.cursor()
+	        command = "INSERT INTO accounts VALUES(%s, %s)" %(user,passw)
+	        c_dup.execute(command)
+        except:
+                flash("Username is already taken")
 #==========================================================
 
 #TESTS
@@ -35,22 +40,20 @@ def createAcc(user, passw):
 #Blogs
 #add an id num and make it primary key - use rand num generator
 #==========================================================
-"""
-command = "CREATE TABLE blogs(id INTEGER PRIMARY KEY, username TEXT, title TEXT, body TEXT, timestamp TEXT)"
-c.execute(command)    #run SQL statement
-"""
 
 #Create a blog
 def createBlog(user, title, body):
-	idnum = random.randint(1, 1000)
-	try:
-                time = time.sfrftime("%Y-%m-%d %H:%M:%S")
-		command = "INSERT INTO blogs VALUES(%d,%s,%s,%s,%s)" %(idnum, user, title, body, time)
+                t = time.sfrftime("%Y-%m-%d %H:%M:%S")
+		command = "INSERT INTO blogs VALUES(%s,%s,%s,%s)" %(user, title, body, time)
 		print command
 		c.execute(command)
-	except:
-		flash("Runtime error")
-		#createBlog(user, title, body, time)
+                """
+        except:
+                print "yo"
+		#flash("Runtime error")
+		#createBlog(user, title, body, time)"""
+
+createBlog('"ajdlfkj"', '"dakfhksa"', '"hakdfjklsdjf"')
 
 def updateBlog(idnum, title, body):
 	try:
